4da0efa477b182f2a5af1c83b6384c2e02c7fb4a
RajveerDhillon <46837997+RajveerDhillon@users.noreply.github.com>
2020-11-16 17:09:42 -0500

    Added some newthings
    When the user is logged in, it will display their username at the navigation bar with a href going to profile. If the user is an admin, an admin va item will appear and directs the user to the admin page

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -4,13 +4,16 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
+
 def register(request):
     if request.method == 'POST':
+
+        
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save() 
             #username = form.cleaned_data.get('username')
-            messages.success(request, f'Your accound has been created! You are now able to login')
+            messages.success(request, f'Your account has been created! You are now able to login')
             return redirect('login')
     else:
         form = UserRegisterForm()
